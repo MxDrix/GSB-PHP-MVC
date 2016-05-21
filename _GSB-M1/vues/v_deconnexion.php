cfd7a71200fd9238d9f62344d1a2eeeba28b8856
Richard <richard.chandon@gmail.com>
2016-05-21 19:14:56 +0200

    Deco

--- _GSB-M1/vues/v_deconnexion.php
+++ _GSB-M1/vues/v_deconnexion.php
@@ -5,13 +5,7 @@
         $nom =  $_SESSION['nom'];
         $prenom = $_SESSION['prenom'];
         echo "<b style='font-size:15px;font-family: 'Open Sans',minion pro, sans-serif;'>Deconnexion en cours de ".$nom." ".$prenom."</b>";
-        session_destroy();           
-        if (isset($_COOKIE['log']) && isset($_COOKIE['mdp'])) {
-            unset($_COOKIE['log']);
-            unset($_COOKIE['mdp']);
-            setcookie('log', '', time() - 365*24*3600, null, null, false, true); // empty value and old timestamp
-            setcookie('mdp', '', time() - 365*24*3600, null, null, false, true); // empty value and old timestamp
-        }
+        
 ?>
 </ul>
 </div>
